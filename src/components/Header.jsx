b3dfe86394a05456631e19261a05684f983c7114
al1211 <kanil013623@gmail.com>
2025-06-10 16:08:46 +0530

    notification complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
+
 
 const Header = () => {
   return (
-<header className="flex items-center justify-between px-5 py-8  border-b-1  bg-white">
+<header className="flex items-center justify-between px-5 py-8  bg-white">
         <div className="flex gap-2">
             
             <button className="bg-green-50 px-3 border rounded-full text-green-900 uppercase text-sm font-bold"> Ready To Interview</button>
         </div>
         <div className="flex gap-2">
             <div className="flex items-center justify-center gap-2">
-            {/* <PiNotificationLight /> */}
-            <p className="text-lg font-medium">Notification</p>
+        
+            <p className="text-lg font-medium flex items-center justify-center"><span className='relative bg-red-600 h-[20px] w-[20px] flex items-center justify-center  rounded-full -top-3 text-white left-2'>1</span>Notification</p>
             </div>
-           {/* <img className="h-10 w-10 border rounded-full" src={user} alt=""/>   */}
+        
         </div>
     </header>
   )
