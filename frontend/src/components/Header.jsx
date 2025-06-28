656fa2888c7c09c0b9770085dc1c76d8715919fb
al1211 <kanil013623@gmail.com>
2025-06-28 12:29:42 +0530

    sussesfull text ai intregation

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,18 +4,24 @@ import SearchAi from "./SearchAi";
 const Header = () => {
   const [notification, setNotification] = useState(1);
   return (
-    <header className="flex items-center justify-between px-5 py-8  bg-white">
-     <SearchAi/>
-        <div className="flex items-center justify-end gap-2">
-          <p className="text-lg font-medium flex items-center justify-center">
-            <span className="relative bg-red-600 h-[20px] w-[20px] flex items-center justify-center  rounded-full -top-3 text-white left-2">
-              {notification}
-            </span>
-            Notification
-          </p>
-        </div>
-      
-    </header>
+  <header className="flex items-center justify-between px-5 py-8 bg-white shadow">
+
+
+  <div className="flex items-center w-1/2 max-w-md flex-col">
+  <SearchAi/>
+
+   
+  </div>
+
+  {/* Notification */}
+  <p className="text-lg font-medium flex items-center relative">
+    <span className="absolute bg-red-600 h-5 w-5 flex items-center justify-center text-xs rounded-full text-white -top-2 left-[70px]">
+      {notification}
+    </span>
+    Notification
+  </p>
+</header>
+
   );
 };
 
--- frontend/src/components/SearchAi.jsx
+++ frontend/src/components/SearchAi.jsx
@@ -1,21 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useTransition } from 'react'
 import {generateText} from "../../api/api"
+import { FiX } from "react-icons/fi";
 const SearchAi = () => {
     const [prompt,setPrompt]=useState("");
     const [response,setResponse]=useState("");
+    const[show,setShow]=useState(false);
+    const [loading,setLoading]=useState(false)
     const handleSumbit=async(e)=>{
       e.preventDefault();
+      if(!prompt.trim()) return;
+      setLoading(true)
+      setResponse("");
      
     try{
+
      let result= await generateText(prompt);
-     console.log(result.data.reply);
-    }catch(err){console.log(err)}
+     if(result){
+      setShow(true);
+     }
+     setResponse(result.data.reply);
+    }catch(err)
+    {console.log(err)}
+    setPrompt("");
+    setLoading(false);
     }
   return (
-  <form  className='flex items-center justify-center' onSubmit={handleSumbit}>
-    <input className='border-2 rounded-lg mr-4 border-gray-400 focus:outline-none px-2 w-full py-2' placeholder='Ask Anything' name='prompt' onChange={(e)=>setPrompt(e.target.value) } value={prompt}/>
-    <button className='inline bg-indigo-900 w-56 rounded-full py-2 text-white'> Ask Gemini</button>
+    <div className='w-full  z-10 flex flex-col items-center  '>
+
+  <form  className='flex items-center justify-center gap-5' onSubmit={handleSumbit}>
+    <input className="w-full px-4 py-2 border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600" placeholder='Ask Anything' name='prompt' onChange={(e)=>setPrompt(e.target.value) } value={prompt} autoComplete='off'/>
+   {loading? (<div className="flex justify-center items-center">
+      <div className="w-12 h-12 border-4 border-indigo-500 border-dashed rounded-full animate-spin"></div>
+    </div>) :  (<button className='bg-indigo-900 w-56 rounded-full py-2 text-white'> Ask Gemini</button>)}
   </form>
+   {show && <div className="bg-gray-800 w-1/2 h-auto z-10 absolute top-20 rounded-2xl p-5 ">
+   <button onClick={()=>setShow(false)} className='text-right w-full flex  justify-end '><FiX className='text-white text-3xl  mb-5 mr-2'/></button>
+  <p className="text-white font-sans text-lg">{response}</p>
+ </div>}
+ 
+    </div>
   )
 }
 
